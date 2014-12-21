5bfdbf1a791191744a9e5a3b314ebe9678130f90
Nash <nash@nashlincoln.com>
2014-12-20 16:33:34 -0800

    add device should take an 'interconnect'

--- app/src/main/java/com/nashlincoln/blink/ui/AddDeviceFragment.java
+++ app/src/main/java/com/nashlincoln/blink/ui/AddDeviceFragment.java
@@ -30,25 +30,26 @@ import java.util.List;
  * Created by nash on 12/6/14.
  */
 public class AddDeviceFragment extends BlinkListFragment
-        implements AdapterView.OnItemClickListener, LoaderManager.LoaderCallbacks<List<DeviceType>> {
+        implements AdapterView.OnItemClickListener {
     private static final String TAG = "AddDeviceFragment";
-    private DeviceType mDeviceType;
+//    private DeviceType mDeviceType;
+    private String mInterconnect;
     private DeviceTypeAdapter mAdapter;
 
     @Override
     protected void onFabClick(View view) {
-        if (mDeviceType != null) {
+        if (mInterconnect != null) {
 //            Intent intent = new Intent();
 //            intent.putExtra(BlinkApp.EXTRA_ID, mDeviceType.getId());
 //            getActivity().setResult(Activity.RESULT_OK, intent);
             Device device = new Device();
-            device.setDeviceType(mDeviceType);
-            device.setInterconnect("ZIGBEE");
+//            device.setDeviceType(mDeviceType);
+            device.setInterconnect(mInterconnect);
             device.setAttributableType(Device.ATTRIBUTABLE_TYPE);
             device.setState(BlinkApp.STATE_ADDED);
             BlinkApp.getDaoSession().getDeviceDao().insert(device);
             Syncro.getInstance().syncDevices();
-            Log.d(TAG, "add: " + mDeviceType.getId());
+            Log.d(TAG, "add: " + device.getId());
             getActivity().finish();
         }
     }
@@ -67,36 +68,39 @@ public class AddDeviceFragment extends BlinkListFragment
         mAdapter = new DeviceTypeAdapter(getActivity());
         mListView.setAdapter(mAdapter);
         mListView.setOnItemClickListener(this);
-        getLoaderManager().initLoader(0, null, this);
+        mAdapter.add("ZIGBEE");
+//        getLoaderManager().initLoader(0, null, this);
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         DeviceTypeAdapter.Holder holder = (DeviceTypeAdapter.Holder) view.getTag();
-        DeviceType deviceType = mAdapter.getItem(holder.position);
-        if (deviceType.isEnabled()) {
-            mDeviceType = deviceType;
-            mAdapter.notifyDataSetChanged();
-        }
+        mInterconnect = mAdapter.getItem(holder.position);
+        mAdapter.notifyDataSetChanged();
+//        DeviceType deviceType = mAdapter.getItem(holder.position);
+//        if (deviceType.isEnabled()) {
+//            mDeviceType = deviceType;
+//            mAdapter.notifyDataSetChanged();
+//        }
     }
 
-    @Override
-    public Loader<List<DeviceType>> onCreateLoader(int id, Bundle args) {
-        return new DeviceTypeLoader(getActivity());
-    }
+//    @Override
+//    public Loader<List<DeviceType>> onCreateLoader(int id, Bundle args) {
+//        return new DeviceTypeLoader(getActivity());
+//    }
+//
+//    @Override
+//    public void onLoadFinished(Loader<List<DeviceType>> loader, List<DeviceType> data) {
+//        mAdapter.clear();
+//        mAdapter.addAll(data);
+//    }
+//
+//    @Override
+//    public void onLoaderReset(Loader<List<DeviceType>> loader) {
+//
+//    }
 
-    @Override
-    public void onLoadFinished(Loader<List<DeviceType>> loader, List<DeviceType> data) {
-        mAdapter.clear();
-        mAdapter.addAll(data);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<List<DeviceType>> loader) {
-
-    }
-
-    class DeviceTypeAdapter extends ArrayAdapter<DeviceType> {
+    class DeviceTypeAdapter extends ArrayAdapter<String> {
 
         public DeviceTypeAdapter(Context context) {
             super(context, 0);
@@ -118,11 +122,11 @@ public class AddDeviceFragment extends BlinkListFragment
         }
 
         private void bindView(Holder holder, int position) {
-            DeviceType deviceType = getItem(position);
+            String interconnect = getItem(position);
             holder.position = position;
-            holder.radioButton.setChecked(deviceType == mDeviceType);
-            holder.textView.setText(deviceType.getDescription());
-            holder.radioButton.setEnabled(deviceType.isEnabled());
+            holder.radioButton.setChecked(interconnect.equals(mInterconnect));
+            holder.textView.setText(interconnect);
+//            holder.radioButton.setEnabled(deviceType.isEnabled());
         }
 
         class Holder {
